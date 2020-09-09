d624d2edf1a51544ed0ab389623aac62d83ceddc
kuczkam <42166328+kuczkam@users.noreply.github.com>
2020-09-09 12:50:16 +0200

    Update CategoriesFilter.vue

--- src/components/CategoriesFilter.vue
+++ src/components/CategoriesFilter.vue
@@ -1,11 +1,52 @@
 <template>
     <div class="container">
-
+        <ul>
+            <li v-for="(list, index) in meals" :key="index">
+                <input 
+                type="radio" 
+                class="hide" 
+                v-model="filterCategories" 
+                :id="list.strCategory" 
+                :value="list.strCategory" 
+                />
+                <label :for="list.strCategory">{{ list.strCategory }}</label>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 export default {
-    
+    computed: {
+        ...mapGetters({
+            meals: 'allMeals'
+        }),
+        filterCategories: {
+            get() {                
+                return this.$store.state.meals
+            },
+            set(category) {
+                this.$store.dispatch('FILTERED_MEALS_BY_CATEGORY', category)
+            }
+        }
+    }
 }
 </script>
+<style scoped>
+ul {
+    display: grid;
+    grid-template-columns:repeat(25, 1fr);
+    grid-gap:1em;
+    list-style-type: none;
+}
+
+.hide {
+    display: none;
+}
+
+label {
+    cursor: pointer;
+}
+</style>
