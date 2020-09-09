1d6412c3b674a30c5d0229d0c7744dc5ceba33c5
kuczkam <42166328+kuczkam@users.noreply.github.com>
2020-09-09 12:53:42 +0200

    Update List.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
       <SearchBar />
+      <div>
+        <AreasFilter />
+      </div>
+      <div>
+        <CategoriesFilter />
+      </div>
+      <div>
+        <TagsFilter />
+      </div>               
     <ul>
       <li v-for="(list, index) in filteredMeals" :key="index">
         <p>{{ list.strMeal }}</p>
+        <p>{{ list.strCategory }}</p>
+        <p>{{ list.strArea }}</p>
+        <p>{{ list.strTags }}</p>
       </li>
     </ul>
     </div>
@@ -11,17 +23,23 @@
 
 <script>
 import SearchBar from './SearchBar'
+import AreasFilter from './AreasFilter'
+import CategoriesFilter from './CategoriesFilter'
+import TagsFilter from './TagsFilter'
 
 export default {
   components: {
-    SearchBar
+    SearchBar,
+    AreasFilter,
+    CategoriesFilter,
+    TagsFilter
   },
   mounted() {
     this.$store.dispatch('GET_MEALS_LIST')
   } ,
   computed: {
     filteredMeals () {
-        let filtred = (this.$store.getters.getFilteredMeals|| this.$store.getters.allMeals)
+        let filtred = (this.$store.getters.getFilteredMeals || this.$store.getters.allMeals)
         return filtred
     }
   }  
